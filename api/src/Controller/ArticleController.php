84f2c31c38212ba44652c713fd779076d0b8eb3b
N-Mohamad <nakshoomar@gmail.com>
2023-12-04 09:37:46 +0100

    controller add

---
+++ api/src/Controller/ArticleController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends AbstractController
+{
+    #[Route('/article', name: 'app_article')]
+    public function index(): Response
+    {
+        return new Response("Hello")  ;  
+    }
+}
