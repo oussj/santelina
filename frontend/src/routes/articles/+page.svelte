ad994af8a016a0cecd46022444e96f7ff9b543bf
N-Mohamad <nakshoomar@gmail.com>
2023-12-04 09:03:10 +0100

    connect backend w frontend

---
+++ frontend/src/routes/articles/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+  import axios from 'axios';
+  import { onMount } from 'svelte';
+
+  let message = 'Chargement...';
+
+  onMount(async () => {
+    try {
+      const response = await axios.get('http://localhost:8000/article');
+      message = response.data;
+    } catch (error) {
+      console.error(error);
+      message = 'Erreur lors du chargement des données';
+    }
+  });
+</script>
+
+<article>
+  <h1>{message}</h1>
+</article>
